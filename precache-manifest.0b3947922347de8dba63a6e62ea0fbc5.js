e79af4db43b17ac820cbff7baa47d69ad0782e62
Ankon <44019225+luciferankon@users.noreply.github.com>
2019-04-03 00:22:49 +0530

    Updates

---
+++ precache-manifest.0b3947922347de8dba63a6e62ea0fbc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9d05e43578a5ddb80d1",
+    "url": "/game-of-life/static/js/runtime~main.dfaa7c2d.js"
+  },
+  {
+    "revision": "d56cf9df658e11332618",
+    "url": "/game-of-life/static/js/main.28fe900e.chunk.js"
+  },
+  {
+    "revision": "91bbf5a2f2e353c2f845",
+    "url": "/game-of-life/static/js/2.2ec475fb.chunk.js"
+  },
+  {
+    "revision": "d56cf9df658e11332618",
+    "url": "/game-of-life/static/css/main.4d33bc59.chunk.css"
+  },
+  {
+    "revision": "811db337f499584b32099cb99e8be8ce",
+    "url": "/game-of-life/index.html"
+  }
+];
